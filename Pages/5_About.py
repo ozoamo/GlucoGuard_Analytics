77e9c1558abc930a105129331ea6af0c9c1cc54d
Brian Nyeko <brian.nyekomoini@gmail.com>
2024-10-15 19:38:02 +0200

    update to about.py

--- Pages/5_About.py
+++ Pages/5_About.py
@@ -8,7 +8,7 @@ st.markdown("""
             1. Soh JGS, Wong WP, Mukhopadhyay A, Quek SC, Tai BC. Predictors of 30-day unplanned hospital readmission among adult patients with diabetes mellitus: a systematic review with meta-analysis. BMJ Open Diabetes Res Care [Internet]. 2020;8(1):e001227. Available from: http://dx.doi.org/10.1136/bmjdrc-2020-001227 
             2. Rubin DJ. Hospital readmission of patients with diabetes. Curr Diab Rep [Internet]. 2015;15(4):17. Available from: http://dx.doi.org/10.1007/s11892-015-0584-7
             3. Zisman-Ilani Y, Fasing K, Weiner M, Rubin DJ. Exercise capacity is associated with hospital readmission among patients with diabetes. BMJ Open Diabetes Res Care [Internet]. 2020;8(1):e001771. Available from: http://dx.doi.org/10.1136/bmjdrc-2020-001771
-            
+            4. UCI machine learning repository [Internet]. Uci.edu. [cited 2024 Oct 15]. Available from: https://archive.ics.uci.edu/dataset/34/diabetes 
 
             **Group members**: Pau Garcia I Morales,Brian Anyau Nyeko Moini,Aurelia Maria Ozora,Dharani Ranasinghe,Muna Mohammad Ahmad Shati,Prabhu Raj Singh
             
